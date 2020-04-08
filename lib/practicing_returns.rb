92d851a27535b5e630e1eaccbe618b9965f8705b
Steph W <wilhelm.steph@gmail.com>
2020-04-08 17:48:12 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -9,5 +9,15 @@ def hello(array)
   end
 end
 
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
